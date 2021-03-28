a98c07325b7de1b29f4acd541486e152b2ccbd6a
Konrad Słowik <konradslowik1998@gmail.com>
2021-03-28 21:11:38 +0200

    Add project files.

---
+++ WebApplication1/Controllers/MoviesController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult ListMovies()
+        {
+            List<Movie> movies = new List<Movie>();
+            movies.Add(new Movie {FilmName = "Shrek"});
+            movies.Add(new Movie { FilmName = "Dom z papieru" });
+
+
+            return View(movies);
+        }
+        public ActionResult Info(int id)
+        {
+            return Content ("id=" + id);
+        }
+    }
+}
